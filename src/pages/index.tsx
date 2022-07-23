e244a4c4913e81fea9e824afd53e41aff3ed73fd
Wandgleyson Lira de Figueredo <gleysonlf@gmail.com>
2022-07-23 18:53:41 -0300

    feat(index): create contact section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ const HomePage = () => {
     <View>
       <div className='flex w-full h-full flex-col items-center justify-center'>
         <SectionAbout />
+        <SectionContact />
       </div>
     </View>
   );
@@ -85,6 +86,95 @@ const SectionAbout = () => (
             Enviar é mais que ir para outra nação, é manifestar a luz de Deus em
             todos os lugares.
           </p>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+const SectionContact = () => (
+  <section className='w-full pb-16 bg-slate-200 relative pt-32'>
+    <div
+      className='-mt-20 top-0 bottom-auto left-0 right-0 w-full absolute h-20'
+      style={{ transform: 'translateZ(0)' }}
+    >
+      <svg
+        className='absolute bottom-0 overflow-hidden'
+        xmlns='http://www.w3.org/2000/svg'
+        preserveAspectRatio='none'
+        version='1.1'
+        viewBox='0 0 2560 100'
+        x='0'
+        y='0'
+      >
+        <polygon
+          className='text-slate-200 fill-current'
+          points='2560 0 2560 100 0 100'
+        ></polygon>
+      </svg>
+    </div>
+
+    <div className='container mx-auto'>
+      <div className='flex flex-wrap justify-center bg-white shadow-xl rounded-lg -mt-64 py-4 px-4 md:py-16/px-12 relative z-10'>
+        <div className='w-full text-center lg:w-8/12'>
+          <div className='relative flex flex-col min-w-0 break-words w-full my-6 shadow-lg rounded-lg bg-slate-200'>
+            <div className='flex-auto p-5 lg:p-10'>
+              <h4 className='text-2xl font-semibold'>
+                Gostaria de nos conhecer melhor?
+              </h4>
+              <p className='leading-relaxed mt-1 mb-4 text-slate-500'>
+                Preencha o formulário e nós entramos em contato.
+              </p>
+              <div className='relative w-full mb-3 mt-8'>
+                <label
+                  className='block uppercase text-slate-600 text-xs font-bold mb-2'
+                  htmlFor='full-name'
+                >
+                  Nome
+                </label>
+                <input
+                  type='text'
+                  className='border-0 px-3 py-3 placeholder-slate-500 text-slate-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150'
+                  placeholder='Como prefere ser chamado(a)'
+                />
+              </div>
+              <div className='relative w-full mb-3'>
+                <label
+                  className='block uppercase text-slate-600 text-xs font-bold mb-2'
+                  htmlFor='email'
+                >
+                  Email
+                </label>
+                <input
+                  type='email'
+                  className='border-0 px-3 py-3 placeholder-slate-500 text-slate-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150'
+                  placeholder='Qual seu e-mail pra contato'
+                />
+              </div>
+              <div className='relative w-full mb-3'>
+                <label
+                  className='block uppercase text-slate-600 text-xs font-bold mb-2'
+                  htmlFor='message'
+                >
+                  Mensagem
+                </label>
+                <textarea
+                  rows={4}
+                  cols={80}
+                  className='border-0 px-3 py-3 placeholder-slate-500 text-slate-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full'
+                  placeholder='Escreva aqui sua mensagem...'
+                />
+              </div>
+              <div className='text-center mt-6'>
+                <button
+                  className='bg-slate-800 text-white active:bg-slate-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                  type='button'
+                >
+                  Enviar
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
